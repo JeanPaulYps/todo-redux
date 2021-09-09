536d5558498888243b9546500531e4f6ee1995ec
Jean Paul <paul.yepesg@perficient.com>
2021-09-09 13:31:07 -0500

    Se tiene el primer MVP de la lista de tareas

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -4,24 +4,18 @@ const TodoContext = React.createContext();
 
 function TodoProvider (props)
 {
-    
+    let initialTodoList = [];
+    const todosLocalStorageName = 'TODOS_V1';
+    if (localStorage.getItem(todosLocalStorageName) === null)
+    {
+      localStorage.setItem(todosLocalStorageName, "[]");
+    } else 
+    {
+      initialTodoList = localStorage.getItem(todosLocalStorageName); 
+      initialTodoList = JSON.parse(initialTodoList);
+    }
 
-    const [todos, setTodos] = React.useState(
-      [ 
-        {
-          description: "Cortar cebollas",
-          completed: false
-        },
-        {
-          description: "Sacar basura",
-          completed: false
-        },
-        {
-          description: "Sacar el perro",
-          completed: true
-        },
-      ]
-    )
+    const [todos, setTodos] = React.useState( initialTodoList );
   
     const [openModal, setOpenModal ] = React.useState(false);
   
@@ -31,11 +25,24 @@ function TodoProvider (props)
         let newTodos = [...todos];
         newTodos[indexOfTask].completed = !statusOftask;
         setTodos(newTodos);
+        localStorage.setItem(todosLocalStorageName, JSON.stringify(newTodos));
     }
 
     const deleteTask = (taskDescription)=>{
       let newTodos = todos.filter((todo) => todo.description !== taskDescription);
       setTodos(newTodos);
+      localStorage.setItem(todosLocalStorageName, JSON.stringify(newTodos));
+    }
+
+    const addTask = (taskDescription) => 
+    {
+        const newTodo = {
+            description: taskDescription,
+            completed: false
+        }
+        let newTodos = [...todos, newTodo]
+        setTodos(newTodos);
+        localStorage.setItem(todosLocalStorageName, JSON.stringify(newTodos));
     }
 
     const [searchText, setSearchText] = React.useState('');
@@ -59,6 +66,7 @@ function TodoProvider (props)
           searchText,
           setSearchText,
           searchedTodos,
+          addTask
         } }>
             {props.children}
         </TodoContext.Provider>
--- src/TodoModal/Modal.js
+++ src/TodoModal/Modal.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './Modal.css';
 
 function Modal(props) {
+    const {addTask} = React.useContext(TodoContext);
+    const [taskDescription, setTaskDescription ]= React.useState('');
+
     const onClickCloseButton = () => 
     {
         props.setOpenModal( prevState => !prevState )
     }
+
+    const handleSubmit = (event) => 
+    {
+        addTask(taskDescription);
+        props.setOpenModal( prevState => !prevState);
+        
+    }
+
+    const saveTaskDescription = (event) =>
+    {
+        setTaskDescription(event.target.value);
+    }
+
 
     return (
         <div className="Modal" id="modal">
@@ -12,12 +30,14 @@ function Modal(props) {
                 <img src={process.env.PUBLIC_URL + "/closeButton.svg"} alt="" onClick= { onClickCloseButton } className="Modal__CloseButtonIcon"/>
                 <div className="CreateTodo">
                     <p className="CreateTodo__Title">Escribe un nuevo To-Do</p>
-                    <textarea className="CreateTodo__TextArea">
-                    </textarea>
-                    <div className="CreateTodo__Buttons">
-                        <button className="CreateTodo__Cancel" type="button">Cancelar</button>
-                        <button className="CreateTodo__Accept" type="button">Aceptar</button>
-                    </div>
+                    <form onSubmit={addTask}> 
+                        <textarea className="CreateTodo__TextArea" name="todoDescription" onChange={saveTaskDescription}>
+                        </textarea>
+                        <div className="CreateTodo__Buttons">
+                            <button className="CreateTodo__Cancel" type="button" onClick={ onClickCloseButton }>Cancelar</button>
+                            <button className="CreateTodo__Accept" type="button" onClick={ handleSubmit }  > Agregar tarea </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
